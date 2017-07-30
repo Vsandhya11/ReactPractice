9d0f39e2c1f9f9c2cd80ba6f2f6b565cbd60f3f5
Sandhya Vemulapalli <sandhya.vemulapalli@rackspace.com>
2017-07-30 15:49:31 -0500

    React Hello World

---
+++ app/index.js
@@ -0,0 +1,18 @@
+var React = require('react');
+var ReactDom = require('react-dom');
+require('./index.css');
+// state
+// lifecycle event
+// UI
+class App extends React.Component {
+ render(){
+   return(
+     <div>Hello React Training!</div>
+   )
+ }
+}
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app')
+);
